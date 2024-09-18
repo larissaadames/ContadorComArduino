b2d3f16e7103f0499daa0299f22909e0acdee193
Larissa Adames Gonçalves <114102618+larissaadames@users.noreply.github.com>
2024-09-18 11:17:54 -0300

    falta a parte do arduino

--- teste.py
+++ teste.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import controller as cnt
 
 video = cv2.VideoCapture(0)
 
@@ -24,12 +25,17 @@ while True:
         dedos = [8, 12, 16, 20]
         contador = 0
         if points:
+            if pontos[4][0] < pontos[2][0]:
+                contador +=1
+                
+            # if pontos[4][0] > pontos[2][0]:
+            #    contador +=1
             for x in dedos:
                 if pontos[x][1] < pontos [x-2][1]:
                     contador += 1
 
-
-        print(contador)
+        cv2.rectangle(img, (80,10), (200,100), (255,0,0), -1)
+        cv2.putText(img, str(contador), (100,100),cv2.FONT_HERSHEY_SIMPLEX, 4, (255,255,255),5)
 
     cv2.imshow("Imagem", img)
     cv2.waitKey(1)
