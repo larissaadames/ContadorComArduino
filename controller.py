c4131929d6293214119fd80640302317c1307797
Larissa Adames Gonçalves <114102618+larissaadames@users.noreply.github.com>
2024-09-23 09:40:31 -0300

    funcionou!!

--- controller.py
+++ controller.py
@@ -4,46 +4,44 @@ comport='COM7'
 
 board=pyfirmata.Arduino(comport)
 
-
 led_1=board.get_pin('d:8:o')
-led_2=board.get_pin('d:9:o')
-led_3=board.get_pin('d:10:o')
-led_4=board.get_pin('d:11:o')
-led_5=board.get_pin('d:12:o')
+led_2=board.get_pin('d:12:o')
+led_3=board.get_pin('d:11:o')
+led_4=board.get_pin('d:10:o')
+led_5=board.get_pin('d:9:o')
 
-def led(fingerUp):
-    if fingerUp==[0,0,0,0,0]:
+def led(total):
+    if total==0:
         led_1.write(0)
         led_2.write(0)
         led_3.write(0)
         led_4.write(0)
         led_5.write(0)
-
-    elif fingerUp==[0,1,0,0,0]:
+    elif total==1:
         led_1.write(1)
         led_2.write(0)
         led_3.write(0)
         led_4.write(0)
         led_5.write(0)
-    elif fingerUp==[0,1,1,0,0]:
+    elif total==2:
         led_1.write(1)
         led_2.write(1)
         led_3.write(0)
         led_4.write(0)
-        led_5.write(0)    
-    elif fingerUp==[0,1,1,1,0]:
+        led_5.write(0)
+    elif total==3:
         led_1.write(1)
         led_2.write(1)
         led_3.write(1)
         led_4.write(0)
         led_5.write(0)
-    elif fingerUp==[0,1,1,1,1]:
+    elif total==4:
         led_1.write(1)
         led_2.write(1)
         led_3.write(1)
         led_4.write(1)
         led_5.write(0)
-    elif fingerUp==[1,1,1,1,1]:
+    elif total==5:
         led_1.write(1)
         led_2.write(1)
         led_3.write(1)
