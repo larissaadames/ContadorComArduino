f4a6827f3d2c805f175c3b6a1174cda7ad063dc0
Larissa Adames Gonçalves <114102618+larissaadames@users.noreply.github.com>
2024-09-23 09:46:06 -0300

    portas

--- controller.py
+++ controller.py
@@ -5,10 +5,10 @@ comport='COM7'
 board=pyfirmata.Arduino(comport)
 
 led_1=board.get_pin('d:8:o')
-led_2=board.get_pin('d:12:o')
-led_3=board.get_pin('d:11:o')
-led_4=board.get_pin('d:10:o')
-led_5=board.get_pin('d:9:o')
+led_2=board.get_pin('d:9:o')
+led_3=board.get_pin('d:10:o')
+led_4=board.get_pin('d:11:o')
+led_5=board.get_pin('d:12:o')
 
 def led(total):
     if total==0:
